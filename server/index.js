29ad5ab99e034bf6e5df950b4b0a615951d834ce
Sandesh Acharya <mysandesh@gmail.com>
2023-01-28 00:18:13 +0545

    Setting up GraphQL, Schema, QueryType.

--- server/index.js
+++ server/index.js
@@ -5,8 +5,19 @@ require("dotenv").config();
 //Bring in GraphQL HTTP
 const { graphqlHTTP } = require("express-graphql");
 
+//Bring in Schema
+const schema = require("./schema/schema");
+
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`Server running on port ${port}`));
