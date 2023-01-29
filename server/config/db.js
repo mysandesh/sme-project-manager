6c897eda29ee41815a0a430f4fa2f422fc7547f6
Sandesh Acharya <mysandesh@gmail.com>
2023-01-29 23:09:36 +0545

    Fix for Mongoose DB Connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
-
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-  const conn = await mongoose.connect("process.env.MONGO_URI");
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useFindAndModify: false,
+    });
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
