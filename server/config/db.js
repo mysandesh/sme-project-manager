a646b1fa7b782bdd0ba61db9298f5fc4f7e2ab08
Sandesh Acharya <mysandesh@gmail.com>
2023-02-01 22:43:24 +0545

    Updated description for Mongoose DB connection fix

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+
+//Includes fix for Mongoose update
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
