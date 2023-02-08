cb1ba3395d6c2c54d6a512be55fc3bcaf7feee08
Sandesh Acharya <mysandesh@gmail.com>
2023-02-08 21:44:11 +0545

    Added Apollo Provider and InMemoryCache

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello World!</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <h1>Hello World!</h1>
+        </div>
+      </ApolloProvider>
     </>
   );
 }
