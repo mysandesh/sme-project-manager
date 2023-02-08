32ad1deb90f0d784c550b282cf9b9b4930cf5ce1
Sandesh Acharya <mysandesh@gmail.com>
2023-02-08 21:37:38 +0545

    Added Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello World!</h1>
+      </div>
+    </>
   );
 }
 
