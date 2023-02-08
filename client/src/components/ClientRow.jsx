e490fa0ec70230d1499efce5143c0dcfa61fe699
Sandesh Acharya <mysandesh@gmail.com>
2023-02-08 22:34:26 +0545

    Added Fetch and Display Clients Functionality

---
+++ client/src/components/ClientRow.jsx
@@ -0,0 +1,16 @@
+import { FaTrash } from "react-icons/fa";
+
+export default function ClientRow({ client }) {
+  return (
+    <tr>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm">
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
+  );
+}
