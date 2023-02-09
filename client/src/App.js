a99798fc71987e51cc91c49e080adabb2e6ff714
Sandesh Acharya <mysandesh@gmail.com>
2023-02-09 21:02:23 +0545

    Setup Apollo Cache

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,25 @@
 import Header from "./components/Header";
-import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Clients from "./components/Clients";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
