3c5cca8ff880ed5b44623e300e26c6facae3d231
Sandesh Acharya <mysandesh@gmail.com>
2023-02-10 21:22:24 +0545

    Addition of Add Client Modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
+import AddClientModal from "./components/AddClientModal";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -32,6 +33,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
---
+++ client/src/components/AddClientModal.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+
+export default function AddClientModal() {
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        data-bs-toggle="modal"
+        data-bs-target="#addClientModal"
+      >
+        <div className="d-flex align-items-center">
+          <FaUser className="icon" />
+          <div>Add Client</div>
+        </div>
+      </button>
+
+      <div
+        className="modal fade"
+        id="addClientModal"
+        aria-labelledby="addClientModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="addClientModalLabel">
+                Add Client
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label className="form-label">Name</label>
+                  <input type="text" className="form-control" id="name" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
