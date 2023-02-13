1a0f2dc914a94ab72e29280f83eada9a40af8bf8
Sandesh Acharya <mysandesh@gmail.com>
2023-02-13 20:44:11 +0545

    Added Fetch and Display Projects with improved views

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,12 +1,18 @@
 export default function ProjectCard({ project }) {
   return (
     <>
-      <div className="col-md-4">
+      <div className="col-md-6">
         <div className="card mb-3">
           <div className="card-body">
             <div className="d-flex justify-content-between align-items-center">
               <h5 className="card-title">{project.name}</h5>
+              <a className="btn btn-light" href={`/projects/${project.id}`}>
+                View
+              </a>
             </div>
+            <p className="small">
+              Status: <strong>{project.status}</strong>
+            </p>
           </div>
         </div>
       </div>
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -12,7 +12,7 @@ export default function Projects() {
   return (
     <>
       {data.projects.length > 0 ? (
-        <div className="row">
+        <div className="row mt-4">
           {data.projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
