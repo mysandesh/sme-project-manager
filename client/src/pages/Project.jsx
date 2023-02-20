56a7094cfe8ee5a23382027416891048b5452c6b
Sandesh Acharya <mysandesh@gmail.com>
2023-02-20 13:46:52 +0545

    Added Project Mutations Fix

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,6 +1,8 @@
 import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 
@@ -23,6 +25,8 @@ export default function Project() {
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
           <ClientInfo client={data.project.client} />
+          <EditProjectForm project={data.project} />
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
