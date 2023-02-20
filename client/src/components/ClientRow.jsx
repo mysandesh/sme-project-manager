bf9d208cf7349d495a00bf1f751dc79892b5c97c
Sandesh Acharya <mysandesh@gmail.com>
2023-02-20 13:34:15 +0545

    Added Delete Project

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,24 +2,25 @@ import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
     //Method 1: Fetch the data again, works but risks app being bogged down
     // refetchQueries: [{ query: GET_CLIENTS }],
 
     //Method 2: Better approach, updating cache, prevents overload
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
   return (
     <tr>
