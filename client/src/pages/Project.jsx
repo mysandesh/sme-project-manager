d1dbe12f4a3b0c55e08cc04564de949994239aa4
Sandesh Acharya <mysandesh@gmail.com>
2023-02-20 13:16:41 +0545

    Added single project queries

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,3 +1,30 @@
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import ClientInfo from "../components/ClientInfo";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECT } from "../queries/projectQueries";
+
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something went wrong</p>;
+
+  return (
+    <>
+      {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
+            Back
+          </Link>
+          <h1>{data.project.name}</h1>
+          <p>{data.project.description}</p>
+          <h5 className="mt-3">Project Status</h5>
+          <p className="lead">{data.project.status}</p>
+          <ClientInfo client={data.project.client} />
+        </div>
+      )}
+    </>
+  );
 }
